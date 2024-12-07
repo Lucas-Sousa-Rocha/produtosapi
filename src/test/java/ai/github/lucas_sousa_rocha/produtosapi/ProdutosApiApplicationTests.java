2ebe9896af3aad23d62ff1651d4775300289bd84
Lucas <lucas.rocha2021@outlook.com>
2024-12-07 11:47:42 -0300

    Realizando o commit do projeto inicial

---
+++ src/test/java/ai/github/lucas_sousa_rocha/produtosapi/ProdutosApiApplicationTests.java
@@ -0,0 +1,13 @@
+package ai.github.lucas_sousa_rocha.produtosapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProdutosApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
